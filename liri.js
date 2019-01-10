d00fb3122b7a2c88eb72bafccf8c0c607d3556cb
Sydney Dunlap <sydneydunlap@Sydneys-MacBook-Pro.local>
2019-01-10 16:17:23 -0500

    added spotify command and it is functional

--- liri.js
+++ liri.js
@@ -1,8 +1,8 @@
 // read and set any environment variables with the dotenv package
 require("dotenv").config();
-// const keys = require("./keys.js");
-// const Spotify = require("node-spotify-api");
-// const spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
 const axios = require("axios");
 const moment = require("moment");
 // const inquirer = require("inquirer");
@@ -12,6 +12,7 @@ const fs = require("fs");
 var nodeArgs = process.argv;
 var artist = "";
 var movieName = "";
+var track = "";
 
 //////// STEP ONE -- currently broken
 
@@ -25,7 +26,9 @@ if (process.argv[2] === "concert-this") {
         function (response) {
             console.log(response);
             // If the request with axios is successful
+            console.log("--------------------");
             console.log("Here are upcoming events for " + artist + ":");
+            console.log("--------------------");
             console.log(moment(response.data[0].datetime).format("MM/DD/YYYY, h:mm a"));
             console.log("Venue: " + response.data[0].venue.name);
             console.log("Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region + ", " + response.data[0].venue.country);
@@ -38,23 +41,35 @@ if (process.argv[2] === "concert-this") {
     );
 }
 
-//////// STEP TWO
+//////// STEP TWO: COMPLETE
+if (process.argv[2] === "spotify-this-song") {
+    track = nodeArgs.slice(3).join(" ");
 
-// node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
+    if (process.argv[3] === undefined) {
+        track = "the sign";
+    }
 
+    spotify.search({
+        type: "track",
+        query: track,
+        limit: 1
+    })
+    .then(function(response) {
+        console.log("Information for the song '" + track + "':");
+        console.log("--------------------");
+        console.log("Song Title:" + response.tracks.items[0].name);
+        console.log("Artist: " + response.tracks.items[0].artists.name);
+        console.log("Album: " + response.tracks.items[0].album.name);
+        console.log("Link: " + response.tracks.items[0].external_urls.spotify);
+        console.log("--------------------");
+    })
+}
 // Artist(s)
 // The song's name
 // A preview link of the song from Spotify
 // The album that the song is from
 
 // If no song is provided then your program will default to "The Sign" by Ace of Base.
-// You will utilize the node-spotify-api package in order to retrieve song information from the Spotify API.
-// The Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a client id and client secret:
-// Step One: Visit https://developer.spotify.com/my-applications/#!/
-// Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-// Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-// Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
 
 /////// STEP THREE: COMPLETE
 
